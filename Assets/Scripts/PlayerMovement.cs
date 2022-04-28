bdde6505050cb889cc6a8987a82dc6f727540870
Benjamin Chilson <benjameschilson@gmail.com>
2022-04-28 01:22:29 -0500

    registering hits on enemy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,7 +20,11 @@ public class PlayerMovement : MonoBehaviour
 
     // References
     private CharacterController controller;
-    private Animator anim;
+    public Animator anim;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
 
     private void Start()
     {
@@ -103,10 +107,16 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private IEnumerator Attack(){
-        anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 1);
+        //anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 1);
         anim.SetTrigger("Attack");
 
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider enemy in hitEnemies){
+            Debug.Log("Hit!");
+        }
+
         yield return new WaitForSeconds(1.9f);
-        anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 0);
+        //anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 0);
     }
 }
